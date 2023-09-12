263b505df7d330fe021aea36518f842da39fc0dc
Emilia Bazan <e.bazanazargado@gmail.com>
2023-09-12 04:11:20 -0300

    Agrego modulos y npm (incompleto)

---
+++ app.js
@@ -0,0 +1,6 @@
+// const greet = require('./greet')
+
+// console.log(greet.greet("mundo"))
+// console.log(greet.bye("mundo"))
+
+
---
+++ greet.js
@@ -0,0 +1,9 @@
+function greet(name) {
+    return `¡Hola ${name}!`
+};
+
+function bye(name) {
+    return `¡Bye ${name}!`
+}
+
+module.exports = { greet, bye }
