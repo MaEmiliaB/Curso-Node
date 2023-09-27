9db71e0446aebbf6d423bdc8c53ae6c33d4731e3
Emilia Bazan <e.bazanazargado@gmail.com>
2023-09-26 21:00:32 -0300

    add cositas

---
+++ ejercicios/ejercicos_clase1.js
@@ -0,0 +1,29 @@
+//1. Suma de los números pares: Escriba un pseudocódigo que calcule la suma de los números pares del 1 al 10.
+
+/*console.log("1.")
+
+var suma = 0;
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        suma += i
+    }
+}
+
+
+console.log(suma)*/
+
+// 2. Tabla de multiplicar: Escriba un pseudocódigo que muestre la tabla de multiplicar del número 5.
+
+// console.log(2.)
+// var producto = 0;
+// for (var i = 1; i <= 10; i++) {
+//     console.log(producto = i * 5)
+
+
+//3. Ordenar una lista: Escriba un pseudocódigo que ordene una lista de números de forma ascendente.
+
+
+/*4. Números primos: Escriba un pseudocódigo que verifique si un número es primo o no.
+5. Conversión de temperaturas: Escriba un pseudocódigo que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+6. Dígitos de un número: Escriba un pseudocódigo que cuente el número de dígitos de un número entero dado.
+7. Potencias: Escriba un pseudocódigo que calcule la potencia de un número dado.
